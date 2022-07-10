aee1c435113f8d329f4a7bdd76d4a3f174d3779a
Razor <therazor113@gmail.com>
2022-07-10 17:51:51 -0400

    Edit for auto selecting a button on load

--- components/pageRefs/DeviceSetup/SelectButtons/index.js
+++ components/pageRefs/DeviceSetup/SelectButtons/index.js
@@ -1,33 +1,65 @@
+import { useEffect, useRef } from 'react'
 import classes from './styles.module.scss'
 
 const SelectButtons = () => {
+  const typeRef = useRef()
+  const networkRef = useRef()
+
+  useEffect(() => {
+    typeRef.current.checked = 'checked'
+    networkRef.current.checked = 'checked'
+  }, [])
+
+  const handleReweigh = () => {
+    
+  }
+
+  const handleDMS = () => {
+    
+  }
+
+  const handleBoth = () => {
+    
+  }
+
+  const handleWifi = () => {
+    
+  }
+
+  const handleCellular = () => {
+    
+  }
+
+  const handleAuto = () => {
+    
+  }
 
   return (
     <main className={classes.container}>
       <h2>Type</h2>
       <div>
-        <input type='radio' name='type' />
+        <input ref={typeRef} onClick={handleReweigh} type='radio' name='type' />
         <button>Reweigh</button>
       </div>
       <div>
-        <input type='radio' name='type' />
+        <input onClick={handleDMS} type='radio' name='type' />
         <button>DMS</button>
       </div>
       <div>
-        <input type='radio' name='type' />
+        <input onClick={handleBoth} type='radio' name='type' />
         <button>Both</button>
       </div>
       <h2>Network</h2>
       <div>
-        <input type='radio' name='network' />
+        <input ref={networkRef} onClick={handleWifi} type='radio' name='network' />
         <button>Wifi</button>
       </div>
       <div>
-        <input type='radio' name='network' />
+        <input onClick={handleCellular} type='radio' name='network' />
         <button>Cellular</button>
       </div>
       <div>
-        <input type='radio' name='network' />
+        <input onClick={handleAuto} type='radio' name='network' />
         <button>Auto</button>
       </div>
     </main>
