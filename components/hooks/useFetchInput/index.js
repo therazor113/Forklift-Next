3476c37f8abcd5c4a9bd3c1febe326bde0e88d20
Razor <therazor113@gmail.com>
2022-07-10 09:57:17 -0400

    Edit to useFetchInput make more modular

--- components/hooks/useFetchInput/index.js
+++ components/hooks/useFetchInput/index.js
@@ -1,29 +1,15 @@
 import { useState } from 'react'
-import { useRouter } from 'next/router'
 
-const useFetchInput = (initialValues, route, url, setData1, setData2) => {
+const useFetchInput = (initialValues, url) => {
   const [inputValue, setInputValue] = useState(initialValues)
-  const router = useRouter()
+  const [data, setData] = useState(undefined)
 
-  const handleFetch = async () => {
-    try {
-     // Fetch data from url + inputValue
-      const req = await fetch(url + inputValue)
-     // Convert response to json
-      const data = await req.json()
-     // Sets Data1 to data fetched
-      setData1(data)
-     // Adds new data fetched to Data2
-     if (setData2) {
-      setData2(prevData => [...prevData, data])
-     }
-     // Send to route location
-      router.push(route)
-    } catch {
-     // Error
-      console.log('error')
-    }
-  }
+ const handleFetch = () => {
+  fetch(url + inputValue)
+  .then(res => res.json())
+  .then(json => setData(json))
+  .catch(err => console.log(err))
+ }
 
   return [
     inputValue,
@@ -33,21 +19,30 @@ const useFetchInput = (initialValues, route, url, setData1, setData2) => {
     () => {
       handleFetch()
     },
-    setInputValue
+    setInputValue,
+    data
   ]
 }
 
 export default useFetchInput
 
 /**
- * example:
- * 
  * import useFetchInput from './hooks/useFetchInput'
  * 
- * const [inputValue, {set value of input box function}, {run fetch function}, setInputValue] = useFetchInput(
- *  {initial value of input} {route to send to}, {data to set}, {url to send fetch request})
- * =>
- * const [inputValue, handleChange, handleEnter, setInputValue] = useFetchInput(
- *  '', '/loginConfirm', setUserData, `api/NamesApi/[users]/`)
+ * const [
+ * inputValue, 
+ * {set value of input box function}, 
+ * {run fetch function}, setInputValue, 
+ * {json data retrieved}
+ * ] = useFetchInput({initial value of input}, {url to send fetch request})
+ * 
+ * example:
+ * const [
+ * inputValue, 
+ * handleChange, 
+ * handleEnter, 
+ * setInputValue, 
+ * data
+ * ] = useFetchInput('', `api/NamesApi/[users]/`)
  * 
  */
