e7c549f01ad121438ea152eed44e56155baf4db6
Razor <therazor113@gmail.com>
2022-07-12 14:18:50 -0400

    shortened max amount of characters for input

--- components/hooks/useEditInput/index.js
+++ components/hooks/useEditInput/index.js
@@ -2,12 +2,13 @@ import { useState } from 'react'
 
 const useEditInput = initialInput => {
   const [inputValue, setInputValue] = useState(initialInput)
-
+  
 return [
   inputValue,
   e => {
+    if (e.target.value.length == 8) return
     setInputValue({
-      ...inputValue, [e.target.name]: e.target.value})
+      ...inputValue, [e.target.name]: e.target.value.toUpperCase()})
     }
   ]
 }
