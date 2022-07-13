745f186be7d665edea70f4bcbcc95cbd6ae8bf7e
Razor <therazor113@gmail.com>
2022-07-13 10:08:10 -0400

    Added workComplete file

---
+++ pages/workComplete.js
@@ -0,0 +1,11 @@
+import Layout from "../components/core/Layout"
+
+const workComplete = () => {
+  return (
+    <Layout style={{backgroundColor: 'white', color: 'black'}}>
+      <h2>Work Complete</h2>
+    </Layout>
+  )
+}
+
+export default workComplete
