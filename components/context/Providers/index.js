47e5caacf84d3a3a64ec91effcceaf17d3c80836
Razor <therazor113@gmail.com>
2022-07-15 11:54:20 -0400

    added TrailerContextProvider

--- components/context/Providers/index.js
+++ components/context/Providers/index.js
@@ -2,18 +2,21 @@ import { UserProvider } from "../UserContext"
 import { WeightProvider } from "../WeightContext"
 import { CurrentProProvider } from "../CurrentProContext"
 import { PreviousProProvider } from "../PreviousProContext"
+import { CurrentTrailerProvider } from "../CurrentTrailerContext"
 
 const Providers = ({ children }) => {
   return (
-    <PreviousProProvider>
-      <UserProvider>
-        <WeightProvider>
-          <CurrentProProvider>
-            {children}
-          </CurrentProProvider>
-        </WeightProvider>
-      </UserProvider>
-    </PreviousProProvider>
+    <CurrentTrailerProvider>
+      <PreviousProProvider>
+        <UserProvider>
+          <WeightProvider>
+            <CurrentProProvider>
+              {children}
+            </CurrentProProvider>
+          </WeightProvider>
+        </UserProvider>
+      </PreviousProProvider>
+    </CurrentTrailerProvider>
   )
 }
 
