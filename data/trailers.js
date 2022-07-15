9552d0977eaee8fd99cc0f371ad052ad7c3a9432
Razor <therazor113@gmail.com>
2022-07-15 11:56:02 -0400

    temp trailer data

---
+++ data/trailers.js
@@ -0,0 +1,38 @@
+export const trailers = [
+  {
+  number: 1234,
+  location: 20,
+  status: 'break',
+  freight: {
+    proNumber: 111111111,
+    shipper: 'shipper',
+    consignee: 'consignee',
+    handlingUnits: 1,
+    pieces: 1,
+    weight: 1,
+    dim: 'N/A',
+    dest: 'N/A',
+    drop: 'N/A',
+    dispatch: 'N/A',
+    final: 'N/A',
+    }
+  },
+  {
+    number: 4321,
+    location: 50,
+    status: 'break',
+    freight: {
+      proNumber: 222222222,
+      shipper: 'shipper',
+      consignee: 'consignee',
+      handlingUnits: 10,
+      pieces: 10,
+      weight: 10,
+      dim: 'N/A',
+      dest: 'N/A',
+      drop: 'N/A',
+      dispatch: 'N/A',
+      final: 'N/A',
+      }
+    }
+]
