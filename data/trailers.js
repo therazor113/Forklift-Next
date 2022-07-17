c38e34c3b0f90737f0ec1dc596f37d18b3b2d898
Razor <therazor113@gmail.com>
2022-07-17 11:02:50 -0400

    Edit to temp trailer data

--- data/trailers.js
+++ data/trailers.js
@@ -3,24 +3,18 @@ export const trailers = [
   number: 1234,
   location: 20,
   status: 'break',
-  freight: {
-    proNumber: 111111111,
-    shipper: 'shipper',
-    consignee: 'consignee',
-    handlingUnits: 1,
-    pieces: 1,
-    weight: 1,
-    dim: 'N/A',
-    dest: 'N/A',
-    drop: 'N/A',
-    dispatch: 'N/A',
-    final: 'N/A',
-    }
+  billCount: 0,
+  pieces: 0,
+  weight: 0,
+  freight: {}
   },
   {
     number: 4321,
     location: 50,
     status: 'break',
+    billCount: 1,
+    pieces: 10,
+    weight: 10,
     freight: {
       proNumber: 222222222,
       shipper: 'shipper',
