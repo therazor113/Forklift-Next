26c0aaf205d89cf9c0869e8243c1c623ce0a5c77
Razor <therazor113@gmail.com>
2022-07-17 11:03:13 -0400

    Imported new files

--- pages/moveConfirmation.js
+++ pages/moveConfirmation.js
@@ -1,10 +1,20 @@
 import Layout from "../components/core/Layout"
 import LiveStats from "../components/pageRefs/WorkComplete/LiveStats"
+import MoveRouting from "../components/pageRefs/WorkComplete/MoveRouting"
+import Exceptions from "../components/pageRefs/WorkComplete/Exceptions"
+
+import classes from '../styles/moveConfirmation.module.scss'
 
 const workComplete = () => {
   return (
     <Layout titleID={'Move Confirmation:'} style={{backgroundColor: 'white', color: 'black'}}>
-      <LiveStats />
+      <main className={classes.container}>
+        <div className={classes.info}>
+          <LiveStats />
+          <Exceptions />
+        </div>
+        <MoveRouting />
+      </main>
     </Layout>
   )
 }
