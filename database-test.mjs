1807136248d917104eee1bbf169c26570138dd6f
Razor <therazor113@gmail.com>
2022-07-18 17:06:44 -0400

    Added temp terminal log of all data in database

---
+++ database-test.mjs
@@ -0,0 +1,18 @@
+import sqlite3 from 'sqlite3'
+import { open } from 'sqlite'
+
+(async () => {
+  try {
+    const db = await open({
+      filename: './tmp/database.db',
+      driver: sqlite3.Database
+    })
+    await db.migrate({ force: true, migrationsPath: './migrations' })
+
+    const trailers = await db.all('SELECT * FROM Trailers')
+    console.log('ALL TRAILERS', trailers)
+
+    const freight = await db.all('SELECT * FROM Freight')
+    console.log('ALL FREIGHT', freight)
+  } catch (err) { console.log(err) }
+})()
