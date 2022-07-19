d71632bc3c5290703340dbc13af4b4d57b015a68
Razor <therazor113@gmail.com>
2022-07-19 15:11:15 -0400

    Added Keypads

---
+++ src/pages/dockKeypad.js
@@ -0,0 +1,12 @@
+import Layout from 'components/core/Layout'
+import DockKeypadTerminal from 'components/pageRefs/DockKeypadTerminal'
+
+const manualProEntry = () => {
+  return (
+    <Layout style={{ backgroundColor: 'white', color: 'black' }}>
+      <DockKeypadTerminal />
+    </Layout>
+  )
+}
+
+export default manualProEntry
---
+++ src/pages/loadKeypad.js
@@ -0,0 +1,12 @@
+import Layout from 'components/core/Layout'
+import LoadKeypadTerminal from 'components/pageRefs/LoadKeypadTerminal'
+
+const manualProEntry = () => {
+  return (
+    <Layout style={{ backgroundColor: 'white', color: 'black' }}>
+      <LoadKeypadTerminal />
+    </Layout>
+  )
+}
+
+export default manualProEntry
