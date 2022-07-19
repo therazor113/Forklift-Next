f27648f475da145845745ffb5e41bf730c7eda6b
Razor <therazor113@gmail.com>
2022-07-19 11:20:14 -0400

    Added users api

---
+++ src/pages/api/UsersApi/[id]/index.js
@@ -0,0 +1,14 @@
+import sqlite3 from 'sqlite3'
+import { open } from 'sqlite'
+
+const getUserById = async (req, res) => {
+  const db = await open({
+    filename: './tmp/users.db',
+    driver: sqlite3.Database
+  })
+  const user = await db.get('SELECT * FROM Users WHERE id = ?', [req.query.id])
+
+  res.json(user)
+}
+
+export default getUserById
