fcc9c13ba972467889d0fefdada3be2b1ad4ed3c
Razor <therazor113@gmail.com>
2022-07-19 11:18:23 -0400

    Removed unused temp data

--- src/pages/api/NamesApi/[users]/[userId].js
+++
@@ -1,7 +0,0 @@
-import { names } from 'data/names'
-
-export default function handler (req, res) {
-  const { userId } = req.query
-  const user = names.find((user) => user.id === parseInt(userId))
-  res.status(200).json(user)
-}
