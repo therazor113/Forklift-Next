43cd179ac2c1bd8a71921eb58bc0d5c512550780
Razor <therazor113@gmail.com>
2022-08-09 10:50:31 -0400

    Fixed stringify data to work with new db

--- src/components/pageRefs/LoadKeypadTerminal/Enter/index.js
+++ src/components/pageRefs/LoadKeypadTerminal/Enter/index.js
@@ -1,13 +1,13 @@
 export const Enter = async (currentPro, setCurrentPro, inputValue, trailerId) => {
-  const updateRes = await fetch(`api/DatabaseApi/freight/updateLoadedDocked/${currentPro.proNumber}`, {
+  const updateRes = await fetch(`api/DatabaseApi/freight/updateLoadedDocked/${currentPro.pronumber}`, {
     method: 'PUT',
     headers: {
       'Content-Type': 'application/json'
     },
-    body: JSON.stringify({ loaded: inputValue, docked: null, onTrailer: trailerId?.trailerId })
+    body: JSON.stringify({ loaded: inputValue, docked: null, onTrailer: trailerId.trailerid })
   })
   await updateRes.json()
-  const res = await fetch(`api/DatabaseApi/freight/${currentPro.proNumber}`)
+  const res = await fetch(`api/DatabaseApi/freight/${currentPro.pronumber}`)
   const data = await res.json()
   setCurrentPro(data)
 }
