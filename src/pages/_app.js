7eeedc98a6bea4249b23f06c3649f748a26b2303
Razor <therazor113@gmail.com>
2022-08-29 16:41:15 -0400

    Added loading bar

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,9 +1,18 @@
 import Providers from 'contexts/Providers'
+import NextNProgress from 'nextjs-progressbar'
 import 'styles/globals.scss'
 
 const MyApp = ({ Component, pageProps }) => {
   return (
   <Providers>
+      <NextNProgress
+        color="#29D"
+        startPosition={0.3}
+        stopDelayMs={200}
+        height={3}
+        showOnShallow={false}
+        options={{ showSpinner: false }}
+      />
     <Component {...pageProps} />
   </Providers>
   )
